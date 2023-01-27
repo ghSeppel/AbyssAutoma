c7afc47a72c8c8df144071e44b2cded06cec9d85
ghSeppel <stoecklein@web.de>
2023-01-27 22:34:10 +0100

    update

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,82 +1,39 @@
 //***
 // Counter Buttons & Fields
+var iCardCount = 18;
+var cnt = 0;
+var strPath = "./cards/";
+var arrCards = new Array(iCardCount - 1);
 
-$('.btn-number').click(function (e) {
-    e.preventDefault();
-    
-    fieldName = $(this).attr('data-field');
-    type      = $(this).attr('data-type');
-    var input = $("input[name='"+fieldName+"']");
-    var currentVal = parseInt(input.val());
-    if (!isNaN(currentVal)) {
-        if(type == 'minus') {
-            
-            if(currentVal > input.attr('min')) {
-                input.val(currentVal - 1).change();
-            } 
-            if(parseInt(input.val()) == input.attr('min')) {
-                $(this).attr('disabled', true);
-            }
+resetCards();
 
-        } else if(type == 'plus') {
-
-            if(currentVal < input.attr('max')) {
-                input.val(currentVal + 1).change();
-            }
-            if(parseInt(input.val()) == input.attr('max')) {
-                $(this).attr('disabled', true);
-            }
-
-        }
-    } else {
-        input.val(0);
+function nextCard() {
+    cnt += 1;
+    if (cnt > iCardCount) {
+        resetCards();
+        cnt = 0;
     }
-});
-$('.input-number').focusin(function(){
-   $(this).data('oldValue', $(this).val());
-});
-$('.input-number').change(function() {
-    
-    minValue =  parseInt($(this).attr('min'));
-    maxValue =  parseInt($(this).attr('max'));
-    valueCurrent = parseInt($(this).val());
-    
-    name = $(this).attr('name');
-    if(valueCurrent >= minValue) {
-        $(".btn-number[data-type='minus'][data-field='"+name+"']").removeAttr('disabled')
-    } else {
-        alert('Sorry, the minimum value was reached');
-        $(this).val($(this).data('oldValue'));
-    }
-    if(valueCurrent <= maxValue) {
-        $(".btn-number[data-type='plus'][data-field='"+name+"']").removeAttr('disabled')
-    } else {
-        alert('Sorry, the maximum value was reached');
-        $(this).val($(this).data('oldValue'));
-    }
-});
 
-$(".input-number").keydown(function (e) {
-    // Allow: backspace, delete, tab, escape, enter and .
-    if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 190]) !== -1 ||
-         // Allow: Ctrl+A
-        (e.keyCode == 65 && e.ctrlKey === true) || 
-         // Allow: home, end, left, right
-        (e.keyCode >= 35 && e.keyCode <= 39)) {
-             // let it happen, don't do anything
-             return;
-    }
-    // Ensure that it is a number and stop the keypress
-    if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
-        e.preventDefault();
-    }
-});
+    var strCard = "Card" + getNextCard() + ".png";
+    document.getElementById("AutomaCard").src = strPath + strCard;
+}
 
-// Roll D20 and Reset
-$("#roll").click(function (e) {
-    lngRoll = Math.floor(Math.random() * 20) + 1;
-    $("#lngResult").val(lngRoll).change();
-})
-$("#reset").click(function (e) {
-    $("#lngResult").val("--").change();
-})
+function resetCards() {
+    var i;
+    console.log("reshuffle");
+    for (i=0; i < iCardCount; i++) {
+        arrCards[i] = 0;
+    }
+}
+
+function getNextCard() {
+    var res = 1;
+    var i = 0;
+    while (res == 1) {
+        i = Math.floor(Math.random() * iCardCount);
+        res = arrCards[i];
+        //console.log(i + " - " + res);
+    }
+    arrCards[i] = 1;
+    return(i+1);
+}
